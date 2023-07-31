32482851d3a1974a0a0d2ab0cb603b13ef06a61a
DreamPrism <thelastprism@qq.com>
2023-07-31 13:44:37 +0800

    Updated version info.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("CSTI Cheat Mode Mod")]
-[assembly: AssemblyDescription("Enable and localize the cheat console for CSTI")]
+[assembly: AssemblyDescription("Cheat mode for CSTI")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CSTI Cheat Mode Mod")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.0.2.0")]
+[assembly: AssemblyFileVersion("0.0.2.0")]
